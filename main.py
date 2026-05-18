acca7baaa4c08e17f8816e8b8a0a33bf964ddca5
NotHuman07 <immuaasid@gmail.com>
2026-05-19 01:23:59 +0530

    Fix CHAT_ID generator bug

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import threading
 import os
 import platform
 from dotenv import load_dotenv
+from src.utils.logger import init_db
+
+init_db()
 
 load_dotenv()
 bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 
-CHAT_ID = (int(chat_id) for chat_id in os.getenv("CHAT_ID").split(","))
-
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 from src.handlers import register_handlers
 from src.services.network import watch_network
 
 register_handlers(bot)
 
-thread = threading.Thread(target=watch_network, args=(bot, CHAT_ID))
+thread = threading.Thread(target=watch_network, args=(bot, ADMIN_ID))
 thread.daemon = True
 
 thread.start()
